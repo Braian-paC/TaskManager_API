ba42278d7079c134e93a4ac5e6227f0dcf8c96e6
Braian-paC <braianbbg4@gmail.com>
2026-07-06 15:31:33 -0300

    Adding post method

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
+from models import Task
 
 router = APIRouter() # Just simplifying in a variable
 
-@router.get("/{id}") # The same thing as 'app'
-def read_root(id: int): # Show the 'id' based on the 'id int' you add to the path
-    return {"id": id}
+@router.get("/") # The same thing as 'app'
+def read_root(id: int = 0, name: str = "Braian"): # Show the 'id' based on the 'id int' you add to the path
+    return {"id": id, "name": name}
+
+@router.post("/tasks") # Validating Task class data
+def create_task(task: Task): # http://127.0.0.1:8000/docs# -> Try it out
+    return task
