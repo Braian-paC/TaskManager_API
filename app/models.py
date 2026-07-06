5d3cee4bfa2d99c7f2c90857b32cd4baf42cf399
Braian-paC <braianbbg4@gmail.com>
2026-07-06 20:25:12 -0300

    Create task and get task

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
-class Task(BaseModel):
+class Task(BaseModel): # Task Model
     id: int
     name: str
+    checked: bool
--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter
 from models import Task
 
 router = APIRouter() # Just simplifying in a variable
+tasks = []
 
-@router.get("/") # The same thing as 'app'
-def read_root(id: int = 0, name: str = "Braian"): # Show the 'id' based on the 'id int' you add to the path
-    return {"id": id, "name": name}
+@router.post("/tasks")
+def create_task(taskModel: Task):
+    tasks.append(taskModel)
+    return taskModel
 
-@router.post("/tasks") # Validating Task class data
-def create_task(task: Task): # http://127.0.0.1:8000/docs# -> Try it out
-    return task
+@router.get("/tasks")
+def get_task():
+    return tasks
