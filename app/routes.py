bdd2fc723d8508bd91387ae154db2d2455437764
Braian-paC <braianbbg4@gmail.com>
2026-07-07 20:18:28 -0300

    Task is created and saved in the database.json

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter
 from models import Task
+from database import read_data, save_data
 
 router = APIRouter() # Just simplifying in a variable
-tasks = []
 
-@router.post("/tasks")
+@router.post("/tasks", response_model=Task)
 def create_task(taskModel: Task): # Create the model Task
-    tasks.append(taskModel)
-    print(tasks)
+    data = read_data()
+    data.append(taskModel.model_dump())
+    save_data(data)
     return taskModel
 
-@router.get("/tasks") # List the Tasks
+@router.get("/tasks", response_model=list[Task]) # List the Tasks
 def get_task():
-    return tasks
+    return read_data()
