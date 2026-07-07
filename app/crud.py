ace83c4aeb13ef080bf7bb80e617dca6d197bcfe
Braian-paC <braianbbg4@gmail.com>
2026-07-07 13:32:26 -0300

    Update CRUD.py with infos

---
+++ app/crud.py
@@ -0,0 +1,9 @@
+'''
+@router.post # Create   - Create a new resource
+
+@router.get # Read      - Read/Search a resource
+
+@router.put # Update    - Update a resource
+
+@router.delete # Delete - Remove a resource
+'''
--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,14 @@ router = APIRouter() # Just simplifying in a variable
 tasks = []
 
 @router.post("/tasks")
-def create_task(taskModel: Task):
+def create_task(taskModel: Task): # Create the model Task
     tasks.append(taskModel)
     return taskModel
 
-@router.get("/tasks")
+@router.get("/tasks") # List the Tasks
 def get_task():
-    return tasks
+    decision = int(input("[1] -> List the tasks\n[2] -> Pass\n"))
+    if decision == 1:
+        return tasks
+    else:
+        pass
