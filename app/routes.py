b9e8aeefaad7d4388b7d1fad7de7dc6588729402
Braian-paC <braianbbg4@gmail.com>
2026-07-07 14:52:38 -0300

    Removing Ternary Operator in get_task()

--- app/routes.py
+++ app/routes.py
@@ -7,12 +7,9 @@ tasks = []
 @router.post("/tasks")
 def create_task(taskModel: Task): # Create the model Task
     tasks.append(taskModel)
+    print(tasks)
     return taskModel
 
 @router.get("/tasks") # List the Tasks
 def get_task():
-    decision = int(input("[1] -> List the tasks\n[2] -> Pass\n"))
-    if decision == 1:
-        return tasks
-    else:
-        pass
+    return tasks
