5f2354b9204c858341a839fa0c0fdfeeb7472168
Braian-paC <braianbbg4@gmail.com>
2026-07-16 14:54:57 -0300

    refactor: Refactoring a part of the code

--- app/crud.py
+++ app/crud.py
@@ -1,9 +1,34 @@
-'''
-@router.post # Create   - Create a new resource
+from models import Task
+from database import read_data, save_data
 
-@router.get # Read      - Read/Search a resource
+def create_task_func(taskModel: Task): # Create a task
+    data = read_data()
+    data.append(taskModel.model_dump())
+    save_data(data)
+    print(data)
+    return taskModel
 
-@router.put # Update    - Update a resource
+def get_task_func(): # Read the database
+    return read_data()
 
-@router.delete # Delete - Remove a resource
-'''
+def update_task_func(id: int, taskModel: Task): # Update a task
+    data = read_data()
+
+    for index, item in enumerate(data):
+        if item["id"] == id:
+            data[index] = taskModel.model_dump()
+            save_data(data)
+            return taskModel
+        
+    return {"error": "Task not found"}
+
+def delete_task_func(id: int): # Delete a task
+    data = read_data()
+
+    for task in data:
+        if task["id"] == id:
+            data.remove(task)
+            save_data(data)
+            return task
+    
+    return {"message": "Task not found"}
--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,22 @@
 from fastapi import APIRouter
 from models import Task
 from database import read_data, save_data
+from crud import create_task_func, get_task_func, update_task_func, delete_task_func
 
-router = APIRouter() # Just simplifying in a variable
+router = APIRouter()
 
-@router.post("/tasks", response_model=Task)
-def create_task(taskModel: Task): # Create the model Task
-    data = read_data()
-    data.append(taskModel.model_dump())
-    save_data(data)
-    print(data)
-    return taskModel
+@router.post("/tasks", response_model=Task) # Create a task
+def create_task(taskModel: Task):
+    return create_task_func(taskModel)
 
-@router.get("/tasks", response_model=list[Task]) # List the Tasks
+@router.get("/tasks", response_model=list[Task]) # Read the database
 def get_task():
-    return read_data()
+    return get_task_func()
 
-@router.put("/tasks/{id}", response_model=Task) # Update task stats
-def update_task(id: int, task: Task):
-    data = read_data()
+@router.put("/tasks/{id}", response_model=Task) # Update a task
+def update_task(id: int, taskModel: Task):
+    return update_task_func(id, taskModel)
 
-    for index, item in enumerate(data):
-        if item["id"] == id:
-            data[index] = task.model_dump()
-            save_data(data)
-            return task
-        
-    return {"error": "Task not found"}
-
-@router.delete("/tasks/{id}")
+@router.delete("/tasks/{id}") # Delete a task
 def delete_task(id: int):
-    data = read_data()
-
-    for task in data:
-        if task["id"] == id:
-            data.remove(task)
-            save_data(data)
-            return task
-    
-    return {"message": "Task not found"}
+    return delete_task_func(id)
